5ad5047d5dda3a3a26adeae66321e1f64bded5e9
Vladimir <samarin.vladimir163@gmail.com>
2024-05-21 15:54:32 +0300

    Initial commit

---
+++ calculator-service/src/test/java/ru/evig/calculatorservice/CalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evig.calculatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
