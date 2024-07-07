69dd4bf6ffb0fe02f6b86959e93214bafcf440da
Samarina <an.samarina@mail.ru>
2024-07-07 17:04:03 +0400

    Initial commit for dossier-service

---
+++ dossier-service/src/main/java/ru/evig/dossierservice/DossierServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.evig.dossierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossierServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DossierServiceApplication.class, args);
+    }
+}
